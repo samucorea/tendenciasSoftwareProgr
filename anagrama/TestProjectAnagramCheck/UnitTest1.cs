831af0bb258684b9cc32f27f60ea4af3a669bcd0
samucorea <samuel.alejandro100@hotmail.com>
2021-05-14 10:39:59 -0400

    s2 is null unit test, tested and working

--- anagrama/TestProjectAnagramCheck/UnitTest1.cs
+++ anagrama/TestProjectAnagramCheck/UnitTest1.cs
@@ -14,5 +14,13 @@ namespace TestProjectAnagramCheck
 
             Assert.That(() => anagram.AreAnagrams(null, "hey"), Throws.ArgumentNullException);
         }
+
+        [Test]
+        public void s2_is_null_throw_ArgumentNullException()
+        {
+            Anagram anagram = new Anagram();
+
+            Assert.That(() => anagram.AreAnagrams("adios", null), Throws.ArgumentNullException);
+        }
     }
 }
